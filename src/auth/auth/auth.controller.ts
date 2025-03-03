277ce5d13b5c82179e6e7ab98ed06fc20c33d415
Max Ivanov <axioma16@gmail.com>
2025-03-03 20:03:28 +0200

    Forgot to add controller to changes)

--- src/auth/auth/auth.controller.ts
+++ src/auth/auth/auth.controller.ts
@@ -1,17 +1,17 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
-@Controller('auth')
+@Controller({ path: 'auth', version: '1' })
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @Post('register')
-  async register(@Body() body: { email: string; password: string }) {
-    return this.authService.register(body.email, body.password);
+  @Post('login')
+  async login(@Body() body) {
+    return this.authService.login(body.username, body.password);
   }
 
-  @Post('login')
-  async login(@Body() body: { email: string; password: string }) {
-    return this.authService.login(body.email, body.password);
+  @Post('register')
+  async register(@Body() body) {
+    return this.authService.register(body.username, body.password);
   }
 }
